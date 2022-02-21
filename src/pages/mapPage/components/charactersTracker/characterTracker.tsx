7faab9f24189e26b8ab234cba53b9951f982e868
Alexandr <torasov.alexandr@gmai.com>
2022-02-21 23:29:17 +0300

    arr characterTracker.tsx

---
+++ src/pages/mapPage/components/charactersTracker/characterTracker.tsx
@@ -0,0 +1,17 @@
+import {connect} from "react-redux";
+import {RootState} from "store/store";
+
+
+function mapStateToProps(state: RootState) {
+    console.log(state)
+    const {map} = state
+    return {map}
+}
+
+export const CharacterTracker = connect(mapStateToProps)(() => {
+    return (
+        <div>
+
+
+        </div>)
+})
--- src/pages/mapPage/index.tsx
+++ src/pages/mapPage/index.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import styles from "./index.module.scss";
 //components
-import { MapField} from "./components/mapField";
-import { MapMenu } from './components/mapMenu';
+import {MapField} from "./components/mapField";
+import {MapMenu} from './components/mapMenu';
+import {CharacterTracker} from "./components/charactersTracker/characterTracker";
 
 export const MapPage = () => {
-  return (
-    <main className={styles.wrapper}>
-      <section>
-        <MapMenu />
-      </section>
-        <section >
-            <MapField />
-      </section>
+    return (
+        <main className={styles.wrapper}>
+            <MapMenu/>
+            <MapField/>
+            <CharacterTracker/>
 
-      {/*<section>chat</section>*/}
-    </main>
-  );
+            {/*<section>chat</section>*/}
+        </main>
+    );
 };
